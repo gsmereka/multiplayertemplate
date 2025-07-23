43163bf0ddff3e7e7727eac86b99710f9f5b5966
Gabriel Smereka <102737099+gsmereka@users.noreply.github.com>
2025-07-22 23:12:56 -0300

    feat: add assets

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -5,30 +5,30 @@ const MOTION_SPEED = 180.0
 
 @export var synced_position : Vector2
 
-@onready
-var inputs = $Inputs
+@onready var inputs = $Inputs
+@onready var camera = $Camera2D
 
 func _ready():
-	print("Original Player spawned")
+	print("Player spawned: ", name)
+
 	if str(name).is_valid_int():
 		get_node("Inputs/InputsSync").set_multiplayer_authority(str(name).to_int())
-		pass
+
+	# Habilita a câmera apenas se este player é o dono (autoridade local)
+	
+	if is_multiplayer_authority():
+		camera.enabled = true
+		print(name + " yes")
+	else:
+		camera.enabled = false
+		print(name + " no")
 
 
 func _physics_process(delta):
-	#inputs.update()
-	#velocity = inputs.motion * MOTION_SPEED
-	#move_and_slide()
-	#look_at(inputs.mouse_pos)
-	#return
-	#print("Multiplayer Peer: ", multiplayer.multiplayer_peer)
-	#if multiplayer.multiplayer_peer == null or str(multiplayer.get_unique_id()) == str(name):
 	if str(multiplayer.get_unique_id()) == str(name):
-		# The client which this player represent will update the controls state, and notify it to everyone.
 		inputs.update()
 
-	#if multiplayer.multiplayer_peer == null or is_multiplayer_authority():
-	if is_multiplayer_authority():# Everybody runs physics. I.e. clients tries to predict where they will be during the next frame.
+	if is_multiplayer_authority():
 		velocity = inputs.motion * MOTION_SPEED
 		move_and_slide()
 		look_at(inputs.mouse_pos)
