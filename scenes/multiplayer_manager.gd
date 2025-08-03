5647a96340f3bc07f022b46e290b1d487afcd32d
Gabriel Smereka <102737099+gsmereka@users.noreply.github.com>
2025-08-02 23:23:02 -0300

    updating spawning

--- scenes/multiplayer_manager.gd
+++ scenes/multiplayer_manager.gd
@@ -7,7 +7,6 @@ var	_players_in_game: Dictionary = {}
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	await  get_tree().process_frame
-
 	if GameState.is_hosting_game:
 		multiplayer.peer_connected.connect(_client_connected)
 		multiplayer.peer_disconnected.connect(_client_disconnected)
@@ -16,26 +15,23 @@ func _ready():
 		pass
 
 func _add_player_to_game(network_id):
-	print("Adding player to game: ", str(network_id))
+	#print("Adding player to game: ", str(network_id))
 	
 	var	player_to_add = _player_scene.instantiate()
 	player_to_add.name = str(network_id);
-	_ready_player(player_to_add)
+	player_to_add.visible = false
+	player_to_add.global_position = $"../PlayerSpawn".global_position
 	_players_in_game[network_id] = player_to_add
-	_player_spawn_point.add_child(player_to_add)
+	_player_spawn_point.call_deferred("add_child",player_to_add)
 	pass
 
 func _remove_player_to_game(network_id):
-	print("Removing player of game: ", str(network_id))
+	#print("Removing player of game: ", str(network_id))
 	if _players_in_game.has(network_id):
 		var player_to_remove = _players_in_game[network_id]
 		if player_to_remove:
 			player_to_remove.queue_free()
 			_players_in_game.erase(network_id)
-	pass
-
-func _ready_player(player: Node2D):
-	player.position = Vector2(randi_range(-2, 2), randi_range(-2, 2))
 	pass
 
 func _client_connected(network_id):
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -5,22 +5,38 @@ const MOTION_SPEED = 180.0
 @export var is_local_player := false
 @export var color : Color
 # Nós
-@onready var inputs = $Inputs
-@onready var states = $States
-@onready var camera = $Camera2D 
-@onready var collision_shape = $CollisionShape2D
+@export var inputs : Node
+@export var states : Node
+@export var camera : Camera2D 
+@export var collision_shape : CollisionShape2D
+@export var canvas : CanvasLayer
+@export var interface : Control
 
-func _ready():
+func _enter_tree():
+	set_multiplayer_authority(str(name).to_int())
+	is_local_player = is_multiplayer_authority()
 	if is_local_player:
+		hide()
+		canvas.visible = true
 		camera.enabled = true
-		return
-	if str(name).is_valid_int():
-		is_local_player = str(multiplayer.get_unique_id()) == str(name)
-		states.set_multiplayer_authority(str(name).to_int())
-		inputs.set_multiplayer_authority(str(name).to_int())
+
+func _ready() -> void:
 	if is_local_player:
-		camera.enabled = true
+		_setup_player()
+
+func _setup_player():
+	await get_tree().create_timer(2.0).timeout
+	global_position = get_parent().global_position
+	$CanvasLayer/Control/Loading.hide()
+	show()
+	pass
 
 func _physics_process(delta):
 	if is_local_player:
 		inputs.update()
+
+#func _physics_process(delta):
+	#if Input.is_action_just_pressed("space") && is_local_player:
+		#global_position = get_parent().global_position
+	#if is_local_player:
+		#inputs.update()
