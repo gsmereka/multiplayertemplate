de5571524ce9d66e32b896e24680fa61db3c4fab
Gabriel Smereka <102737099+gsmereka@users.noreply.github.com>
2025-08-05 21:51:33 -0300

    feat: refact player

--- scenes/objects/object.gd
+++ scenes/objects/object.gd
@@ -4,10 +4,11 @@ class_name ObjectClass
 
 @export var hp = 3
 
-@rpc("any_peer", "reliable", "call_local")
+@rpc("any_peer", "call_local")
 func take_damage():
+	if !multiplayer.is_server():
+		return
 	if hp > 0:
 		hp-= 1
 	if hp <= 0:
-		if multiplayer.is_server():
-			queue_free()
+		queue_free()
