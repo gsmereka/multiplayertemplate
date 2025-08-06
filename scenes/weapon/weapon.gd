315819081262251e0ee375026e080ba37bae70d4
Gabriel Smereka <102737099+gsmereka@users.noreply.github.com>
2025-08-06 15:44:11 -0300

    fix: fira just on rpc_id

--- scenes/weapon/weapon.gd
+++ scenes/weapon/weapon.gd
@@ -10,7 +10,7 @@ func Fire():
 		if cast.is_colliding():
 			var col := cast.get_collider()
 			if col is Player:
-				col.take_damage.rpc()
+				col.take_damage.rpc_id(col.name.to_int())
 			elif col.has_method("take_damage"):
 				col.take_damage.rpc_id(1)
 	await get_tree().create_timer(0.1).timeout
