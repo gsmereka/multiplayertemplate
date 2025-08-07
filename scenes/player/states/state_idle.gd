b7fd33fe1497620d2550b377f6c860d6731f1396
Gabriel Smereka <102737099+gsmereka@users.noreply.github.com>
2025-08-06 21:29:02 -0300

    feat: add camera slide on player

--- scenes/player/states/state_idle.gd
+++ scenes/player/states/state_idle.gd
@@ -19,8 +19,26 @@ func Update(_delta:float):
 	#if inputs:
 	player.velocity = inputs.motion * MOTION_SPEED
 	player.look_at(inputs.mouse_pos)
+	update_camera_position()
 	player.move_and_slide()
 	pass
+	
+@export var max_camera_offset := 100.0  # Distância máxima que a câmera pode se afastar do player em direção ao mouse	
+
+func update_camera_position():
+	if not player.camera:
+		return
+
+	var mouse_global_pos = inputs.mouse_pos
+	var player_pos = player.global_position
+
+	var direction = (mouse_global_pos - player_pos).normalized()
+	var distance = min((mouse_global_pos - player_pos).length(), max_camera_offset)
+
+	var target_offset = direction * distance
+	var target_camera_pos = player_pos + target_offset
+
+	player.camera.global_position = player.camera.global_position.lerp(target_camera_pos, 0.1)  # suavemente segue
 
 func Process_update(_delta:float):
 	pass
